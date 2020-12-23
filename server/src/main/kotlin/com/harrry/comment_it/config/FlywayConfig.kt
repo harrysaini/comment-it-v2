d95522a7b585b6ef0f14eae3798a3320858e936c
harish.kumar2 <harish.kumar2@olx.com>
2020-12-23 14:10:28 +0530

    added build

--- server/src/main/kotlin/com/harrry/comment_it/config/FlywayConfig.kt
+++ server/src/main/kotlin/com/harrry/comment_it/config/FlywayConfig.kt
@@ -1,23 +1,19 @@
 package com.harrry.comment_it.config
 
+import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import javax.annotation.PostConstruct
 
 @Configuration
-@EnableConfigurationProperties(value = [DatabaseConfig::class])
 class FlywayConfig (
-        val databaseConfig: DatabaseConfig
+        val hikariDataSource: HikariDataSource
 ) {
 
     @PostConstruct
     fun migrateWithFlyway() {
-        val flyway = Flyway.configure().dataSource(
-                databaseConfig.url,
-                databaseConfig.username,
-                databaseConfig.password
-        ).load()
+        val flyway = Flyway.configure().dataSource(hikariDataSource).load()
 
         flyway.migrate()
     }
